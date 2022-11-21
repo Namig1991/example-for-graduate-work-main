59e8f9a8819100cdde0fb16ebe055f1ab8f5dae8
dgobozov <saniamit@mail.ru>
2022-11-21 21:34:41 +0300

    Заменил на JDBC и раздизэйблил cors()

--- src/main/java/ru/skypro/homework/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/WebSecurityConfig.java
@@ -3,12 +3,10 @@ package ru.skypro.homework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import java.util.Collection;
-import java.util.HashSet;
+
+import javax.sql.DataSource;
 import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class WebSecurityConfig {
@@ -22,34 +20,20 @@ public class WebSecurityConfig {
     };
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user@gmail.com")
-                .password("password")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("estonec@gmail.com")
-                .password("password")
-                .roles("ADMIN")
-                .build();
-        Collection<UserDetails> users = new HashSet<>();
-        users.add(user);
-        users.add(admin);
-        return new InMemoryUserDetailsManager(users);
+    public JdbcUserDetailsManager userDetailsManager (DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeHttpRequests((authz) ->
-                        authz
-                                .mvcMatchers(AUTH_WHITELIST).permitAll()
-                                .mvcMatchers("/ads/**", "/users/**").authenticated()
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests((authz) -> authz
+                               .mvcMatchers(AUTH_WHITELIST).permitAll()
+                               .mvcMatchers("/ads/**", "/users/**").authenticated()
                 )
-                .cors().disable()
+                .cors().and()
                 .httpBasic(withDefaults());
         return http.build();
+
     }
 }
 
