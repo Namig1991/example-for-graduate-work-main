bccb102144bd0e87b89315485eb4b48569ae51f9
dgobozov <saniamit@mail.ru>
2022-11-22 21:55:38 +0300

    Add code in AdsService

--- src/main/java/ru/skypro/homework/service/AdsService.java
+++ src/main/java/ru/skypro/homework/service/AdsService.java
@@ -1,4 +1,26 @@
 package ru.skypro.homework.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.AdsDto;
+import org.springframework.security.core.Authentication;
+import ru.skypro.homework.dto.CreateAdsDto;
+import ru.skypro.homework.dto.FullAdsDto;
+import ru.skypro.homework.dto.ResponseWrapperAdsDto;
+
 public interface AdsService {
+    ResponseWrapperAdsDto getAllAds();
+
+    FullAdsDto getAds(Integer id);
+
+    AdsDto createAds(CreateAdsDto createAdsDto, MultipartFile file, Authentication authentication);
+
+    void removeAds(Integer id, String username, UserDetails userDetails);
+
+    AdsDto updateAdvert(Integer id, AdsDto adsDto, String username, UserDetails userDetails, MultipartFile file);
+
+    ResponseWrapperAdsDto findAds(String search);
+
+    ResponseWrapperAdsDto getAdsMe(String name);
+
 }
